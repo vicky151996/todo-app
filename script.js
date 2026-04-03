b095e6d69807e16e4afc3aedc5e4a1c0aeb1a1bf
vicky151996 <guleriaakash15@gmail.com>
2026-04-03 21:25:38 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
-alert("JS connected");
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function renderTasks() {
+  let list = document.getElementById("taskList");
+  list.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    let li = document.createElement("li");
+
+    let span = document.createElement("span");
+    span.textContent = task.text;
+
+    if (task.completed) {
+      span.classList.add("completed");
+    }
+
+    span.onclick = function () {
+      tasks[index].completed = !tasks[index].completed;
+      saveTasks();
+    };
+
+    let btn = document.createElement("button");
+    btn.textContent = "❌";
+    btn.onclick = function () {
+      tasks.splice(index, 1);
+      saveTasks();
+    };
+
+    li.appendChild(span);
+    li.appendChild(btn);
+    list.appendChild(li);
+  });
+}
+
+function addTask() {
+  let input = document.getElementById("taskInput");
+
+  if (input.value.trim() === "") return;
+
+  tasks.push({
+    text: input.value,
+    completed: false
+  });
+
+  input.value = "";
+  input.focus();
+
+  saveTasks();
+}
+
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  renderTasks();
+}
+
+document.getElementById("taskInput").addEventListener("keypress", function(e) {
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
+
+renderTasks();
